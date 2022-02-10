bf41049d5afc52462e8c58db0788f302b3c5ab61
vikak <vikak@LAPTOP-OI65I849>
2022-02-10 12:40:48 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Демонстрация работы класса:");
+            string str = "Пример текстовой переменной";
+            Company myCompany = new Company();
+            myCompany.name = "Вика";
+            myCompany.persons = 10;
+            myCompany.money = 100000000;
+            Console.WriteLine("имя:"+myCompany.name);
+            Console.WriteLine("количество сотрудников:"+myCompany.persons);
+            Console.WriteLine("зп:"+myCompany.money);
+        }
+    }
+}
